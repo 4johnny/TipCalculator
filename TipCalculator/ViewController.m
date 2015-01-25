6ce9e11fd3078a21c1b1f95e9ba47b7d33103d7a
4johnny <4johnny@users.noreply.github.com>
2015-01-24 16:02:31 -0800

    Initial impl.

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -8,20 +8,31 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
 
+
 @implementation ViewController
+
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)calculateTip {
+
+	self.tipAmountLabel.text = [NSString stringWithFormat:@"%.2f", self.billAmountTextField.text.intValue * 0.15];
+}
+
 
 @end
